da9bd4e1236646adc8820a92fb9858416a3a3128
Rudra Pandya <rudrapandyace29@gmail.com>
2024-09-07 13:55:42 +0530

    b

---
+++ js/home.js
@@ -0,0 +1,9 @@
+// Get "Learn More" button and add event listener
+document.getElementById('learn-more-btn').addEventListener('click', () => {
+    alert('Learn more about our platform coming soon!');
+});
+
+// Get "Sign Up" button and add event listener
+document.getElementById('signup-btn').addEventListener('click', () => {
+    window.location.href = 'signup.html';
+});
